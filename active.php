0486f75df3eb40ee6a54014ee65887278982a5fa
Tripathi Raghu <48356533+tripathiraghu@users.noreply.github.com>
2020-10-01 00:15:57 +0530

    Changes

--- active.php
+++ active.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-	@ob_start();
+@ob_start();
 include_once "connection.php";
-		$uid=$_GET['uid'];
-		//$nvalue=stripslashes(trim($_POST['nvalue']));
-		$active="UPDATE userinfo SET value='1000' where id=$uid";
-echo $active;
+	$uid=$_GET['uid'];
+	//$nvalue=stripslashes(trim($_POST['nvalue']));
+	$active="UPDATE userinfo SET value='1000' where id=$uid";
+echo $acive;
 $add_query=mysqli_query($connect,$active)  or die(mysql_error());
  header("location: alluser.php");
 ?>
