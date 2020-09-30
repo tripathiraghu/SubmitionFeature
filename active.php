2674cbf371694931ff99bb712fa4ba4e76bca78e
gtripathi97 <72140115+gtripathi97@users.noreply.github.com>
2020-09-30 18:04:47 +0530

    Add files via upload

---
+++ active.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+	@ob_start();
+include_once "connection.php";
+		$uid=$_GET['uid'];
+		//$nvalue=stripslashes(trim($_POST['nvalue']));
+		$active="UPDATE userinfo SET value='1000' where id=$uid";
+echo $active;
+$add_query=mysqli_query($connect,$active)  or die(mysql_error());
+ header("location: alluser.php");
+?>
